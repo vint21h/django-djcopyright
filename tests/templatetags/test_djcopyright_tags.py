a2d48b890048250994d3f816228a6ea5697557b7
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2019-09-01 22:25:49 +0300

    Add templatetags tests

---
+++ tests/templatetags/test_djcopyright_tags.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# django-djcopyright
+# tests/templatetags/test_djcopyright_tags.py
+
+
+from typing import List  # pylint: disable=W0611
+
+from django.test import TestCase
+from django.utils import timezone
+
+from djcopyright.templatetags.djcopyright_tags import djcopyright_years
+
+
+__all__ = ["DjcopyrightYearsTemplatetagTest"]  # type: List[str]
+
+
+YEAR = timezone.now().today().year  # type: int
+
+
+class DjcopyrightYearsTemplatetagTest(TestCase):
+    """
+    Get copyright templatetag tests.
+    """
+
+    def test_djcopyright_years(self) -> None:
+        """
+        Must return formatted copyright tag.
+
+        :return: nothing.
+        :rtype: None.
+        """
+
+        result = djcopyright_years()  # type: str
+
+        self.assertIsInstance(obj=result, cls=str)
+        self.assertEqual(first=result, second=str(YEAR))
